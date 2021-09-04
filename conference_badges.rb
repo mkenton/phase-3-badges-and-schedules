0ecd3cf648efda8699f180bbfad348366f5d8437
Michael <mkenton2020@icloud.com>
2021-09-04 01:24:10 -0500

    commit tree 77acb605ab4e4bef7ead075398b7b074b2fd9e07

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_arr)
+    name_arr.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index do |name, index| 
+        "Hello, #{name}! You'll be assigned to room #{index+1}!"  #or use map.with_index(1) and then use #{index}
+    end
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each do |badge|
+        puts badge
+    end
+    assign_rooms(speakers).each do |assignment|
+        puts assignment
+    end
+end
